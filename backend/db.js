00789f1240f626485684844e99647b7e290ec6d2
Govindan007 <sgovindan077@gmail.com>
2025-07-09 21:32:12 +0530

    updated db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config(); // make sure to load .env file
 
-const url = process.env.MONGO_URI; // match this with your .env key
+const url = process.env.mongodb_url; // match this with your .env key
 
 mongoose.connect(url)
   .then(() => {
