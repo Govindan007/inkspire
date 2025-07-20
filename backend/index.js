c637dca107fadb7b1cf704a61b5eafcb1268d90e
mathewjosephta <mathewjoseph3831v@gmail.com>
2025-07-20 13:01:34 +0530

    Signup and login succesfull

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,14 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/admin", require("./routes/adminRoutes"));
 
+app.use("/admin", require("./routes/adminRoutes"));
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/blogs", require("./routes/blogRoutes"));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ DB Connection Error:", err));
-
-const authRoutes = require("./routes/authRoutes");
-app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
