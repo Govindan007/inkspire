e3ae1350c4b1b95991362de81e18441834a5f45a
mathewjosephta <mathewjoseph3831v@gmail.com>
2025-07-22 10:02:07 +0530

    resolved conflict

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-<<<<<<< HEAD
 
 const url = process.env.mongodb_url;
 console.log("Connecting to MongoDB URL:", url); // 👈 Add this
@@ -12,10 +11,3 @@ mongoose.connect(url)
   .catch((error) => {
     console.error("❌ MongoDB connection error:", error);
   });
-=======
-require("dotenv").config();
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => console.error("❌ DB Connection Error:", err));
->>>>>>> 1d59e6172d2f0b7a080d4d82aba4226bd1c2626a
